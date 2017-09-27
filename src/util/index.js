6f0462476fd6bae28fdfb0564f78d585e91e398a
Kav Singh <flakisms@gmail.com>
2017-09-28 01:03:21 +0200

    drop unused inspect fn

--- src/util/index.js
+++ src/util/index.js
@@ -2,22 +2,6 @@
 export const log = console.log.bind(console)
 
 export const warn = console.warn.bind(console)
-
-export const inspect = (instance, groupName, predicate = () => true) => {
-  const name = groupName || `${typeof instance} instance`
-
-  console.groupCollapsed(name)
-
-  /* eslint-disable no-restricted-syntax */
-  for (const key in instance) {
-    if (predicate(key)) {
-      console.log(`${key} :: ${typeof instance[key]} :: ${instance[key]}`)
-    }
-  }
-  /* eslint-enable no-restricted-syntax */
-
-  console.groupEnd(name)
-}
 /* eslint-enable */
 
 export const docReady = (eventName = 'complete') =>
