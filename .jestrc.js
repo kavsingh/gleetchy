02cd2d7c787cd8f4aa4a46d94e6fb4424cce64cf
Kav Singh <flakisms@gmail.com>
2017-10-20 20:09:03 +0200

    clean up jestrc

--- .jestrc.js
+++ .jestrc.js
@@ -4,7 +4,6 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.js'],
   coveragePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
-    '^.+\\.css$': 'identity-obj-proxy',
     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__testMocks__/fileMock.js',
   },
