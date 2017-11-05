df0e60bdb1f24d2266018684db8e6e7dc78b32ba
Kav Singh <flakisms@gmail.com>
2017-11-05 20:53:02 +0100

    commit to dark color scheme

--- src/constants/style.js
+++ src/constants/style.js
@@ -1,13 +1,5 @@
-import color from 'color'
-
-const mod = col =>
-  color(col)
-    .negate()
-    .lighten(4)
-    .hex()
-
-export const COLOR_PAGE = mod('#fff')
-export const COLOR_BODY = mod('#444')
-export const COLOR_EMPHASIS = mod('#000')
-export const COLOR_KEYLINE = mod('#faeeee')
+export const COLOR_PAGE = '#101010'
+export const COLOR_BODY = '#efefef'
+export const COLOR_EMPHASIS = '#fff'
+export const COLOR_KEYLINE = '#195555'
 export const COLOR_ERROR = '#ec6764'
