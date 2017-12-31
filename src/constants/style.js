91827fbfb02aaeb3201e799b18f85346c07213b4
Kav Singh <flakisms@gmail.com>
2017-12-31 13:00:17 +0100

    add layout helper def for styles

--- src/constants/style.js
+++ src/constants/style.js
@@ -3,3 +3,11 @@ export const COLOR_BODY = '#efefef'
 export const COLOR_EMPHASIS = '#fff'
 export const COLOR_KEYLINE = '#195555'
 export const COLOR_ERROR = '#ec6764'
+
+export const LAYOUT_ABSOLUTE_FILL = {
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+}
