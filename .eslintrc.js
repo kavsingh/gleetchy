99c9e6f2288db149e1cdc9437c6927b74d321f03
Kav Singh <flakisms@gmail.com>
2018-03-20 20:33:54 +0100

    granular eslint import rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,25 @@
+const importDev = {
+  'import/no-extraneous-dependencies': [
+    'error',
+    {
+      devDependencies: true,
+      optionalDependencies: false,
+      peerDependencies: false,
+    },
+  ],
+}
+
+const importSrc = {
+  'import/no-extraneous-dependencies': [
+    'error',
+    {
+      devDependencies: false,
+      optionalDependencies: false,
+      peerDependencies: false,
+    },
+  ],
+}
+
 module.exports = {
   parser: 'babel-eslint',
   settings: { 'import/resolver': 'webpack' },
@@ -5,17 +27,23 @@ module.exports = {
   plugins: ['prettier'],
   extends: ['mongrel-react', 'prettier', 'prettier/react'],
   rules: {
+    ...importDev,
     'prettier/prettier': 'warn',
   },
   overrides: [
     {
       files: 'src/**/*.js',
       env: { node: false, browser: true },
+      rules: { ...importSrc },
+    },
+    {
+      files: 'src/indexStatic.js',
+      rules: { ...importDev },
     },
     {
       files: 'src/**/*.test.js',
       env: { jest: true },
-      rules: { 'max-nested-callbacks': 'off' },
+      rules: { ...importDev, 'max-nested-callbacks': 'off' },
     },
     {
       files: 'scripts/bin/**/*.js',
