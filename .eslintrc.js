21c64ee76a7b5f0e75bec41dff83f416f2696b36
Kav Singh <flakisms@gmail.com>
2018-11-20 00:59:00 +0100

    clear out eslint deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,19 @@
-const importDev = {
-  'import/no-extraneous-dependencies': [
-    'error',
-    {
-      devDependencies: true,
-      optionalDependencies: false,
-      peerDependencies: false,
-    },
-  ],
-}
-
-const importSrc = {
-  'import/no-extraneous-dependencies': [
-    'error',
-    {
-      devDependencies: false,
-      optionalDependencies: false,
-      peerDependencies: false,
-    },
-  ],
-}
-
 module.exports = {
-  parser: 'babel-eslint',
-  settings: { 'import/resolver': 'webpack' },
   env: { node: true, browser: false },
   plugins: ['prettier'],
-  extends: ['mongrel-react', 'prettier', 'prettier/react'],
+  extends: ['eslint:recommended', 'prettier'],
   rules: {
-    ...importDev,
-    'react/destructuring-assignment': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'no-console': 'off'
     'prettier/prettier': 'warn',
   },
-  overrides: [
-    {
-      files: 'src/**/*.js',
-      env: { node: false, browser: true },
-      rules: { ...importSrc },
-    },
-    {
-      files: 'src/indexStatic.js',
-      rules: { ...importDev },
-    },
-    {
-      files: 'src/**/*.test.js',
-      env: { jest: true },
-      rules: { ...importDev, 'max-nested-callbacks': 'off' },
-    },
-    {
-      files: 'scripts/bin/**/*.js',
-      rules: { 'no-console': 'off' },
-    },
-  ],
   globals: {
     process: false,
   },
