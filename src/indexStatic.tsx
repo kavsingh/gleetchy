f2ffef4ad1f75c05e0bba7a0815ba0c257462355
Kav Singh <flakisms@gmail.com>
2019-04-08 21:15:29 +0200

    use emotion CacheProvider in SSR

--- src/indexStatic.tsx
+++ src/indexStatic.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { cache } from 'emotion'
+import { CacheProvider } from '@emotion/core'
 import { renderStylesToString } from 'emotion-server'
 import { renderToString } from 'react-dom/server'
 
@@ -8,5 +10,9 @@ import Main from './Main'
 
 export default (initialState: Partial<ApplicationState> = {}) =>
   renderStylesToString(
-    renderToString(<Main store={configureStore(initialState)} />),
+    renderToString(
+      <CacheProvider value={cache}>
+        <Main store={configureStore(initialState)} />,
+      </CacheProvider>,
+    ),
   )
