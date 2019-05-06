41f7f2529ac1e450d4fad3e0241c8d1c0d61f2f4
Kav Singh <flakisms@gmail.com>
2019-05-07 00:26:21 +0200

    clean up ssr

--- src/indexStatic.tsx
+++ src/indexStatic.tsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { cache } from 'emotion'
-import { CacheProvider } from '@emotion/core'
-import { renderStylesToString } from 'emotion-server'
 import { renderToString } from 'react-dom/server'
 
 import { ApplicationState, configureStore } from '~/state/configureStore'
@@ -9,10 +6,4 @@ import { ApplicationState, configureStore } from '~/state/configureStore'
 import Main from './Main'
 
 export default (initialState: Partial<ApplicationState> = {}) =>
-  renderStylesToString(
-    renderToString(
-      <CacheProvider value={cache}>
-        <Main store={configureStore(initialState)} />,
-      </CacheProvider>,
-    ),
-  )
+  renderToString(<Main store={configureStore(initialState)} />)
