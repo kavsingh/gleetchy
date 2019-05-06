643304169617938d48d1ea732d40c240424f53e1
Kav Singh <flakisms@gmail.com>
2019-05-06 21:32:14 +0200

    waveform to hooks

--- src/components/WaveForm/index.tsx
+++ src/components/WaveForm/index.tsx
@@ -1,6 +1,6 @@
+import React, { FunctionComponent, useRef, useEffect, memo } from 'react'
 import colorFn from 'color'
 import { map } from 'ramda'
-import React, { Component } from 'react'
 
 import { COLOR_EMPHASIS, COLOR_KEYLINE } from '~/constants/style'
 import { requireWindowWith } from '~/util/env'
@@ -27,141 +27,118 @@ export interface WaveformProps {
   buffer?: AudioBuffer
 }
 
-interface WaveformState {
-  height: number
-  width: number
+const drawTempWaveform = (
+  context: CanvasRenderingContext2D,
+  width: number,
+  height: number,
+) => {
+  context.fillRect(0, height / 2, width, 1)
 }
 
-class WaveForm extends Component<WaveformProps, WaveformState> {
-  public state = {
-    height: 0,
-    width: 0,
+const drawTimeRegions = (
+  context: CanvasRenderingContext2D,
+  width: number,
+  height: number,
+  timeRegions: number,
+) => {
+  for (let i = 0; i < timeRegions; i += 1) {
+    const x = Math.round(i * 0.25 * width)
+
+    context.moveTo(x, 0)
+    context.lineTo(x, height)
   }
 
-  private pixelRatio: number = 1
-  private canvasNode?: HTMLCanvasElement | null
+  context.stroke()
+}
 
-  public componentDidMount() {
-    const WINDOW = requireWindowWith([['addEventListener']])
+const drawWaveForm = (
+  context: CanvasRenderingContext2D,
+  width: number,
+  height: number,
+  buffer: AudioBuffer,
+) => {
+  const halfHeight = height / 2
+  const leftChannel = normaliseChannel(buffer.getChannelData(0))
+  const rightChannel =
+    buffer.numberOfChannels > 1
+      ? normaliseChannel(buffer.getChannelData(1))
+      : leftChannel
+  const buffStep = buffer.length / width
 
-    if (!WINDOW) {
-      return
-    }
+  for (let i = 0; i < width; i += 1) {
+    const index = Math.floor(i * buffStep)
+    const leftVal = leftChannel[index] * halfHeight
+    const rightVal = rightChannel[index] * halfHeight
 
-    this.pixelRatio = WINDOW.devicePixelRatio || 1
-
-    this.handleResize()
-    WINDOW.addEventListener('resize', this.handleResize)
-  }
-
-  public shouldComponentUpdate(props: WaveformProps, state: WaveformState) {
-    return (
-      this.props.buffer !== props.buffer ||
-      this.state.width !== state.width ||
-      this.state.height !== state.height
-    )
-  }
-
-  public componentDidUpdate() {
-    this.updateWaveForm()
-  }
-
-  public componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWaveForm)
-  }
-
-  public render() {
-    return (
-      <div className={classes.root}>
-        <canvas className={classes.canvas} ref={el => (this.canvasNode = el)} />
-      </div>
-    )
-  }
-
-  private drawTempWaveform(context: CanvasRenderingContext2D) {
-    const { width, height } = this.state
-    context.fillRect(0, height / 2, width, 1)
-  }
-
-  private drawWaveForm(context: CanvasRenderingContext2D) {
-    const { buffer } = this.props
-
-    if (!buffer) {
-      return
-    }
-
-    const { width, height } = this.state
-    const halfHeight = height / 2
-    const leftChannel = normaliseChannel(buffer.getChannelData(0))
-    const rightChannel =
-      buffer.numberOfChannels > 1
-        ? normaliseChannel(buffer.getChannelData(1))
-        : leftChannel
-    const buffStep = buffer.length / width
-
-    for (let i = 0; i < width; i += 1) {
-      const index = Math.floor(i * buffStep)
-      const leftVal = leftChannel[index] * halfHeight
-      const rightVal = rightChannel[index] * halfHeight
-
-      context.fillRect(i, halfHeight - leftVal, 1, leftVal + rightVal)
-    }
-  }
-
-  private drawTimeRegions(context: CanvasRenderingContext2D) {
-    const { timeRegions = 4 } = this.props
-    const { width, height } = this.state
-
-    for (let i = 0; i < timeRegions; i += 1) {
-      const x = Math.round(i * 0.25 * width)
-
-      context.moveTo(x, 0)
-      context.lineTo(x, height)
-    }
-    context.stroke()
-  }
-
-  private updateWaveForm = () => {
-    if (!this.canvasNode) {
-      return
-    }
-
-    const { width, height } = this.state
-    const { color = COLOR_EMPHASIS } = this.props
-    const context = this.canvasNode.getContext('2d')
-
-    if (!context) {
-      return
-    }
-
-    this.canvasNode.width = width * this.pixelRatio
-    this.canvasNode.height = height * this.pixelRatio
-
-    context.scale(this.pixelRatio, this.pixelRatio)
-    context.clearRect(0, 0, width, height)
-    context.fillStyle = color
-    context.strokeStyle = colorFn(COLOR_KEYLINE)
-      .darken(0.06)
-      .hex()
-
-    if (this.props.buffer) {
-      this.drawTimeRegions(context)
-      this.drawWaveForm(context)
-    } else {
-      this.drawTempWaveform(context)
-    }
-  }
-
-  private handleResize = () => {
-    this.setState(() =>
-      this.canvasNode
-        ? {
-            height: this.canvasNode.offsetHeight,
-            width: this.canvasNode.offsetWidth,
-          }
-        : null,
-    )
+    context.fillRect(i, halfHeight - leftVal, 1, leftVal + rightVal)
   }
 }
 
-export default WaveForm
+const updateWaveForm = (
+  canvasNode: HTMLCanvasElement,
+  {
+    color = COLOR_EMPHASIS,
+    pixelRatio = 1,
+    timeRegions = 4,
+    buffer,
+  }: WaveformProps & { pixelRatio: number },
+) => {
+  const context = canvasNode.getContext('2d')
+
+  if (!context) return
+
+  const width = canvasNode.offsetWidth
+  const height = canvasNode.offsetHeight
+
+  canvasNode.width = width * pixelRatio
+  canvasNode.height = height * pixelRatio
+
+  context.scale(pixelRatio, pixelRatio)
+  context.clearRect(0, 0, width, height)
+  context.fillStyle = color
+  context.strokeStyle = colorFn(COLOR_KEYLINE)
+    .darken(0.06)
+    .hex()
+
+  if (buffer) {
+    drawTimeRegions(context, width, height, timeRegions)
+    drawWaveForm(context, width, height, buffer)
+  } else {
+    drawTempWaveform(context, width, height)
+  }
+}
+
+const WaveForm: FunctionComponent<WaveformProps> = ({
+  color,
+  timeRegions,
+  buffer,
+}) => {
+  const WINDOW = requireWindowWith([['addEventListener']])
+  const canvasNodeRef = useRef<HTMLCanvasElement>(null)
+  const pixelRatio = WINDOW ? WINDOW.devicePixelRatio || 1 : 1
+
+  const handleResize = () => {
+    if (canvasNodeRef.current) {
+      updateWaveForm(canvasNodeRef.current, {
+        color,
+        timeRegions,
+        buffer,
+        pixelRatio,
+      })
+    }
+  }
+
+  useEffect(() => {
+    handleResize()
+    WINDOW && WINDOW.addEventListener('resize', handleResize)
+    return () => WINDOW && WINDOW.removeEventListener('resize', handleResize)
+  }, [color, timeRegions, buffer])
+
+  return (
+    <div className={classes.root}>
+      <canvas className={classes.canvas} ref={canvasNodeRef} />
+    </div>
+  )
+}
+
+export default memo(WaveForm)
