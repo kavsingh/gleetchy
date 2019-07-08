35ec6887c8bd416b8d9659e3f2c9afa77df12890
Kav Singh <flakisms@gmail.com>
2019-07-09 01:11:27 +0200

    add mutable type conversion helper

--- src/typings/app/index.d.ts
+++ src/typings/app/index.d.ts
@@ -1,2 +1,13 @@
-declare module '*.png'
-declare module '*.wav'
+declare module '*.png' {
+  const filePath: string
+  export default filePath
+}
+
+declare module '*.wav' {
+  const filePath: string
+  export default filePath
+}
+
+declare type Mutable<T extends { [key: string]: any }, K = keyof T> = {
+  [P in K]: T[P]
+}
