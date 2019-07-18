e4310083d860ccfad8f727bd07685193f27a4431
Kav Singh <flakisms@gmail.com>
2019-07-18 23:12:55 +0200

    use css animation instead of transition

--- src/components/AnimIn/index.tsx
+++ src/components/AnimIn/index.tsx
@@ -1,44 +1,28 @@
-import React, {
-  FunctionComponent,
-  useState,
-  useEffect,
-  memo,
-  ReactNode,
-} from 'react'
-import { css } from '@emotion/core'
+import React, { FunctionComponent, memo, ReactNode } from 'react'
+import { css, keyframes } from '@emotion/core'
+
+const animation = keyframes({
+  from: {
+    opacity: 0,
+    transform: 'translate(0, -4%)',
+  },
+  to: {
+    opacity: 1,
+    transform: 'translate(0, 0)',
+  },
+})
 
 const rootStyle = css({
   height: '100%',
   width: '100%',
-  transition: 'transform 0.3s ease-out, opacity 0.3s ease-out',
-})
-
-const hiddenStyle = css({
   opacity: 0,
-  transform: 'translate(0, -4%)',
-})
-
-const visibleStyle = css({
-  opacity: 1,
-  transform: 'translate(0, 0)',
+  animation: `${animation} 400ms forwards ease-out`,
 })
 
 // For some reason memo does not expose children properly.
 // TODO: kiv type updates
-const AnimIn: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
-  const [isVisible, setIsVisible] = useState(false)
-
-  useEffect(() => {
-    let visibleTimeout: NodeJS.Timeout = setTimeout(() => setIsVisible(true), 0)
-
-    return () => visibleTimeout && clearTimeout(visibleTimeout)
-  }, [])
-
-  return (
-    <div css={[rootStyle, isVisible ? visibleStyle : hiddenStyle]}>
-      {children}
-    </div>
-  )
-}
+const AnimIn: FunctionComponent<{ children: ReactNode }> = ({ children }) => (
+  <div css={rootStyle}>{children}</div>
+)
 
 export default memo(AnimIn)
