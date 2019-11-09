f8d56f1990ad400166dc0f02b76adf9ef2f6c104
Kav Singh <flakisms@gmail.com>
2019-11-09 18:28:51 +0100

    fix lint modification

--- .eslintrc.js
+++ .eslintrc.js
@@ -77,11 +77,6 @@ module.exports = {
         'no-unused-vars': unusedVarsConfig,
         '@typescript-eslint/camelcase': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
-        // Disable to avoid false positives in optional chaining
-        // tsc should catch use of undefined values
-        // https://github.com/typescript-eslint/typescript-eslint/issues/1116
-        'no-undef': 'off',
-        '@typescript-eslint/no-undef': 'off',
       },
     },
     {
@@ -91,6 +86,11 @@ module.exports = {
         'no-unused-vars': 'off',
         '@typescript-eslint/camelcase': camelcaseConfig,
         '@typescript-eslint/no-unused-vars': unusedVarsConfig,
+        // Disable no-undef to avoid false positives in optional chaining
+        // tsc should catch use of undefined values
+        // https://github.com/typescript-eslint/typescript-eslint/issues/1116
+        'no-undef': 'off',
+        '@typescript-eslint/no-undef': 'off',
       },
     },
     {
