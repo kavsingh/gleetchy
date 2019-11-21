b0afa0ecb8497e51cd6c5614e24bb2b8c4503d63
Kav Singh <flakisms@gmail.com>
2019-11-21 23:44:05 +0100

    support optional chaning / nullish coalescing

--- babel.config.js
+++ babel.config.js
@@ -16,6 +16,9 @@ module.exports = ({ env }) => ({
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
+    // TODO: remove these when included with preset-env
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     [
       'babel-plugin-module-resolver',
       {
