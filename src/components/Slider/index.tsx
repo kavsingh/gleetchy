61833518f70783454a63f0dcde5c8b96236d80d5
Kav Singh <flakisms@gmail.com>
2019-12-28 02:34:17 +0100

    slider styled components

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,128 +1,26 @@
 import React, { PureComponent } from 'react'
+import styled from '@emotion/styled'
 import { css } from '@emotion/core'
 import { withTheme } from 'emotion-theming'
 import { clamp } from 'ramda'
 
 import { noop } from '~/util/function'
-import { UITheme } from '~/style/theme'
+import { ThemeProps } from '~/style/theme'
 import SinglePointerDrag, {
   SinglePointerDragState,
 } from '~/components/SinglePointerDrag'
 
-const textStyle = css({
-  flex: '0 0 auto',
-  fontSize: '0.8em',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  whiteSpace: 'nowrap',
-})
-
-const horizontalStyle = css({
-  alignItems: 'stretch',
-  flexDirection: 'row',
-})
-
-const rootStyle = css({
-  display: 'flex',
-  height: '100%',
-  width: '100%',
-})
-
-const verticalStyle = css({
-  alignItems: 'center',
-  flexDirection: 'column',
-})
-
-const labelVerticalStyle = css({
-  height: '1.4em',
-})
-
-const valueVerticalStyle = css({
-  alignItems: 'flex-end',
-  display: 'flex',
-  height: '1.4em',
-})
-
-const labelHorizontalStyle = css({
-  alignItems: 'center',
-  display: 'flex',
-  width: '3em',
-})
-
-const valueHorizontalStyle = css({
-  alignItems: 'center',
-  display: 'flex',
-  width: '3em',
-})
-
-const barContainerStyle = css({
-  flex: '1 1',
-  position: 'relative',
-})
-
-const barContainerVerticalStyle = css({
-  cursor: 'ns-resize',
-  margin: '0.4em auto 0.2em',
-  width: '100%',
-})
-
-const barContainerHorizontalStyle = css({
-  cursor: 'ew-resize',
-  height: '100%',
-  margin: 'auto 0.6em',
-})
-
-const trackStyle = (theme: UITheme) =>
-  css({
-    backgroundColor: theme.colors.keyline,
-    position: 'absolute',
-    zIndex: 1,
-  })
-
-const barStyle = (theme: UITheme) =>
-  css({
-    backgroundColor: theme.colors.emphasis,
-    position: 'absolute',
-    zIndex: 2,
-  })
-
-const trackVerticalStyle = css({
-  bottom: 0,
-  left: '50%',
-  top: 0,
-  width: 1,
-})
-
-const barVerticalStyle = css({
-  bottom: 0,
-  left: 'calc(50% - 1px)',
-  top: 0,
-  width: 3,
-})
-
-const trackHorizontalStyle = css({
-  height: 1,
-  left: 0,
-  right: 0,
-  top: '50%',
-})
-
-const barHorizontalStyle = css({
-  height: 3,
-  left: 0,
-  right: 0,
-  top: 'calc(50% - 1px)',
-})
+type Orientation = 'vertical' | 'horizontal'
+type OrientationProps = { orientation: Orientation }
 
 export interface SliderProps {
   value: number
   defaultValue?: number
-  orient?: 'vertical' | 'horizontal'
+  orientation?: Orientation
   label?: string
   valueLabel?: string
   title?: string
   onChange?(value: number): unknown
-  theme: UITheme
 }
 
 class Slider extends PureComponent<SliderProps> {
@@ -131,73 +29,42 @@ class Slider extends PureComponent<SliderProps> {
   public render() {
     const {
       value,
-      orient = 'vertical',
+      orientation = 'vertical',
       label = '',
       valueLabel = '',
       title = '',
-      theme,
     } = this.props
-    const isVert = orient === 'vertical'
     const offVal = `${(1 - value) * 100}%`
 
     return (
-      <div
-        css={[rootStyle, isVert && verticalStyle, !isVert && horizontalStyle]}
-        title={title}
-      >
-        <div
-          css={[
-            textStyle,
-            isVert && labelVerticalStyle,
-            !isVert && labelHorizontalStyle,
-          ]}
-        >
-          {label}
-        </div>
+      <Container orientation={orientation} title={title}>
+        <Label orientation={orientation}>{label}</Label>
         <SinglePointerDrag
           onDragMove={this.handleDragMove}
           onDragEnd={this.handleDragEnd}
         >
           {({ dragListeners }) => (
-            <div
+            <BarContainer
               {...dragListeners}
-              css={[
-                barContainerStyle,
-                isVert && barContainerVerticalStyle,
-                !isVert && barContainerHorizontalStyle,
-              ]}
+              orientation={orientation}
               role="presentation"
               onDoubleClick={this.handleDoubleClick}
               ref={el => (this.barContainer = el)}
             >
-              <div
-                css={[
-                  trackStyle(theme),
-                  isVert && trackVerticalStyle,
-                  !isVert && trackHorizontalStyle,
-                ]}
+              <Track orientation={orientation} />
+              <Bar
+                orientation={orientation}
+                style={
+                  orientation === 'horizontal'
+                    ? { right: offVal }
+                    : { top: offVal }
+                }
               />
-              <div
-                css={[
-                  barStyle(theme),
-                  isVert && barVerticalStyle,
-                  !isVert && barHorizontalStyle,
-                ]}
-                style={isVert ? { top: offVal } : { right: offVal }}
-              />
-            </div>
+            </BarContainer>
           )}
         </SinglePointerDrag>
-        <div
-          css={[
-            textStyle,
-            isVert && valueVerticalStyle,
-            !isVert && valueHorizontalStyle,
-          ]}
-        >
-          {valueLabel}
-        </div>
-      </div>
+        <ValueLabel orientation={orientation}>{valueLabel}</ValueLabel>
+      </Container>
     )
   }
 
@@ -205,12 +72,10 @@ class Slider extends PureComponent<SliderProps> {
     movementX,
     movementY,
   }: SinglePointerDragState) => {
-    if (!this.barContainer) {
-      return
-    }
+    if (!this.barContainer) return
 
-    const { orient = 'vertical', value, onChange = noop } = this.props
-    const isVert = orient === 'vertical'
+    const { orientation = 'vertical', value, onChange = noop } = this.props
+    const isVert = orientation === 'vertical'
     const movement = isVert ? movementY : movementX
     const dim = isVert
       ? this.barContainer.offsetHeight * -1
@@ -226,12 +91,10 @@ class Slider extends PureComponent<SliderProps> {
     targetX,
     targetY,
   }: SinglePointerDragState) => {
-    if (!this.barContainer) {
-      return
-    }
+    if (!this.barContainer) return
 
-    const { orient = 'vertical', onChange = noop } = this.props
-    const isVert = orient === 'vertical'
+    const { orientation = 'vertical', onChange = noop } = this.props
+    const isVert = orientation === 'vertical'
     const movement = isVert ? movementY : movementX
 
     if (duration > 300 || movement > 4) {
@@ -252,5 +115,112 @@ class Slider extends PureComponent<SliderProps> {
     onChange(defaultValue)
   }
 }
+
+const Container = styled.div<OrientationProps>`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  ${({ orientation }) =>
+    orientation === 'horizontal'
+      ? css`
+          flex-direction: row;
+          align-items: stretch;
+        `
+      : css`
+          flex-direction: column;
+          align-items: center;
+        `}
+`
+
+const Text = styled.div`
+  flex: 0 0 auto;
+  overflow: hidden;
+  font-size: 0.8em;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`
+
+const Label = styled(Text)<OrientationProps>(({ orientation }) =>
+  orientation === 'horizontal'
+    ? css`
+        display: flex;
+        align-items: center;
+        width: 3em;
+      `
+    : css`
+        height: 1.4em;
+      `,
+)
+
+const ValueLabel = styled(Text)<OrientationProps>`
+  display: flex;
+  ${({ orientation }) =>
+    orientation === 'horizontal'
+      ? css`
+          align-items: center;
+          width: 3em;
+        `
+      : css`
+          align-items: flex-end;
+          height: 1.4em;
+        `};
+`
+
+const BarContainer = styled.div<OrientationProps>`
+  position: relative;
+  flex: 1 1;
+  ${({ orientation }) =>
+    orientation === 'horizontal'
+      ? css`
+          height: 100%;
+          margin: auto 0.6em;
+          cursor: ew-resize;
+        `
+      : css`
+          width: 100%;
+          margin: 0.4em auto 0.2em;
+          cursor: ns-resize;
+        `}
+`
+
+const Track = withTheme(styled.div<ThemeProps & OrientationProps>`
+  position: absolute;
+  z-index: 1;
+  background-color: ${({ theme }) => theme.colors.keyline};
+  ${({ orientation }) =>
+    orientation === 'horizontal'
+      ? css`
+          top: 50%;
+          right: 0;
+          left: 0;
+          height: 1px;
+        `
+      : css`
+          top: 0;
+          bottom: 0;
+          left: 50%;
+          width: 1px;
+        `};
+`)
+
+const Bar = withTheme(styled.div<ThemeProps & OrientationProps>`
+  position: absolute;
+  z-index: 2;
+  background-color: ${({ theme }) => theme.colors.emphasis};
+  ${({ orientation }) =>
+    orientation === 'horizontal'
+      ? css`
+          top: calc(50% - 1px);
+          right: 0;
+          left: 0;
+          height: 3px;
+        `
+      : css`
+          top: 0;
+          bottom: 0;
+          left: calc(50% - 1px);
+          width: 3px;
+        `};
+`)
 
 export default withTheme(Slider)
