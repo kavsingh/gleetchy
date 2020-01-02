a43d08a629220b3af86e6caa514d54de461d757b
Kav Singh <flakisms@gmail.com>
2020-01-02 16:48:58 +0100

    rename node ui files

--- src/nodes/audio-effects/delay/index.ts
+++ src/nodes/audio-effects/delay/index.ts
@@ -1,7 +1,7 @@
 import createAudioNode from './create-audio-node'
 import { defaultProps, Props } from './nodeProps'
 import nodeType from './node-type'
-import UI from './UI'
+import UI from './ui'
 
 export type NodeProps = Props
 export { UI, nodeType, defaultProps, createAudioNode }
