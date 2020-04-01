e9852c3b4a762ff99e906a3971c2f07d1e26b048
Kav Singh <flakisms@gmail.com>
2020-04-01 22:38:05 +0200

    use type imports to fix dependency cycle

--- src/state/hooks/use-audio-node-subscription-data.ts
+++ src/state/hooks/use-audio-node-subscription-data.ts
@@ -1,9 +1,7 @@
 import { useSelector } from 'react-redux'
 import { equals } from 'ramda'
 
-// TODO: type only import
-// eslint-disable-next-line import/no-cycle
-import { ApplicationState } from '../configure-store'
+import type { ApplicationState } from '../configure-store'
 
 const useAudioNodeSubscriptionData = (id: string) =>
   useSelector<ApplicationState, { [key: string]: unknown }>(
