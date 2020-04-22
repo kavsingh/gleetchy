3565a3d215e869ec7d5e9dd1a35512dde2fc9092
Kav Singh <flakisms@gmail.com>
2020-04-22 21:22:30 +0200

    redux devtools dev only

--- src/state/configure-store.ts
+++ src/state/configure-store.ts
@@ -1,5 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import thunk from 'redux-thunk'
 import type { Store } from 'redux'
 
