5495f7828aa0733b698a2e7f104c476a77171e2c
Kav Singh <flakisms@gmail.com>
2022-10-24 17:27:41 +0200

    bail from node name change

--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,21 +1,48 @@
-import { useCallback, memo } from "react";
+import { useCallback, memo, useRef } from "react";
 import AutosizeInput from "react-input-autosize";
 import styled from "@emotion/styled";
 import { noop } from "lodash";
 
 import { cancelReactEvent } from "~/lib/util";
 
-import type { ChangeEventHandler, FC } from "react";
+import type {
+	ChangeEventHandler,
+	FC,
+	FocusEventHandler,
+	KeyboardEventHandler,
+} from "react";
 
 const TextInput: FC<{
 	value: string | number;
 	placeholder?: string;
 	onChange?(value: string): unknown;
 }> = ({ value, placeholder = "", onChange = noop }) => {
+	const initialValueRef = useRef(value);
+	const inputRef = useRef<HTMLInputElement | null>(null);
+
+	const handleFocus = useCallback<FocusEventHandler<HTMLInputElement>>(
+		(event) => {
+			initialValueRef.current = event.currentTarget.value;
+		},
+		[],
+	);
+
 	const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
 		(event) => {
 			cancelReactEvent(event);
 			onChange(event.currentTarget.value);
+		},
+		[onChange],
+	);
+
+	const handleKeyDown = useCallback<KeyboardEventHandler>(
+		(event) => {
+			if (!["Enter", "Escape"].includes(event.key)) return;
+
+			event.preventDefault();
+			inputRef.current?.blur();
+
+			if (event.key === "Escape") onChange(initialValueRef.current);
 		},
 		[onChange],
 	);
@@ -26,6 +53,11 @@ const TextInput: FC<{
 				value={value}
 				placeholder={placeholder}
 				onChange={handleChange}
+				onFocus={handleFocus}
+				onKeyDown={handleKeyDown}
+				// TODO: investigate a fix
+				// @ts-expect-error typing conflict AutoSize lib
+				ref={inputRef}
 			/>
 		</Container>
 	);
@@ -36,12 +68,12 @@ export default memo(TextInput);
 const Container = styled.div`
 	input {
 		border: none;
-		border-bottom: 1px solid transparent;
+		border-block-end: 1px solid transparent;
 		color: currentColor;
 		font: inherit;
 		background-color: transparent;
 		cursor: initial;
-		transition: color 0.2s ease-out, border-color 0.2s ease-out;
+		transition: color 0.2s ease-out, border-block-end-color 0.2s ease-out;
 
 		&:hover,
 		&:active,
