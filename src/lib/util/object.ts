8fae652b582fbc9d00aa20fd45b1502be343b523
Kav Singh <flakisms@gmail.com>
2023-07-30 14:11:35 +0200

    remove lodash.omit usage

--- src/lib/util/object.ts
+++ src/lib/util/object.ts
@@ -1,12 +1,16 @@
-import { omit } from "lodash";
-
 /**
  * Returns new object if there are items to be removed, otherwise
  * returns original object
  */
-export function stableOmit<
-	T extends Record<string, unknown>,
-	K extends keyof T,
->(keys: K[], obj: T): Omit<T, K> {
-	return keys.some((key) => key in obj) ? omit(obj, keys) : obj;
+export function stableOmit<T extends object, K extends keyof T>(
+	keys: K[],
+	obj: T,
+) {
+	if (!keys.some((key) => key in obj)) return obj;
+
+	// TODO: see if can type this better
+
+	return Object.fromEntries(
+		Object.entries(obj).filter(([key]) => !keys.includes(key as K)),
+	) as unknown as Omit<T, K>;
 }
