dfc32ccd741baefa5b2e4a68b69d41b68431d2f3
Kav Singh <flakisms@gmail.com>
2023-08-01 23:34:05 +0200

    instruments rack solid

--- src/ui/instruments-rack/index.tsx
+++ src/ui/instruments-rack/index.tsx
@@ -1,60 +1,58 @@
-import { useCallback, memo } from "react";
-import { ErrorBoundary } from "solid-js";
+import { ErrorBoundary, For, Match, Switch } from "solid-js";
 
 import useAddNode from "~/app-store/hooks/use-add-node";
 import useAudioNodesMeta from "~/app-store/hooks/use-audio-nodes-meta";
 import ButtonGroup, { Button } from "~/components/button-group";
+import ErrorMessage from "~/components/error-message";
 import { nodeType as loopType } from "~/nodes/instruments/loop";
 
 import ConnectedLoop from "./connected-loop";
 
-import type { PropsWithChildren } from "react";
+import type { ParentProps } from "solid-js";
 
-export default memo(function InstrumentsRack() {
+export default function InstrumentsRack() {
 	return (
 		<div class="flex w-full flex-col gap-8">
 			<Rack />
 			<AddInstrumentButtons />
 		</div>
 	);
-});
+}
 
 function Rack() {
 	const { instruments } = useAudioNodesMeta();
 
 	return (
-		<>
-			{instruments.map((props) => {
-				switch (props.type) {
-					case loopType:
-						return (
-							<RackMount key={props.id}>
-								<ConnectedLoop id={props.id} />
-							</RackMount>
-						);
-					default:
-						return null;
-				}
-			})}
-		</>
+		<For each={instruments()}>
+			{(instrument) => (
+				<Switch fallback={null}>
+					<Match when={instrument.type === loopType}>
+						<RackMount>
+							<ConnectedLoop id={instrument.id} />
+						</RackMount>
+					</Match>
+				</Switch>
+			)}
+		</For>
 	);
 }
 
 function AddInstrumentButtons() {
 	const addNode = useAddNode();
-	const addLoop = useCallback(() => addNode(loopType), [addNode]);
 
 	return (
 		<ButtonGroup>
-			<Button onClick={addLoop}>Add Loop</Button>
+			<Button onClick={[addNode, loopType]}>Add Loop</Button>
 		</ButtonGroup>
 	);
 }
 
-function RackMount(props: PropsWithChildren) {
+function RackMount(props: ParentProps) {
 	return (
 		<div class="animate-appear opacity-0">
-			<ErrorBoundary>{props.children}</ErrorBoundary>
+			<ErrorBoundary fallback={(e) => <ErrorMessage>{String(e)}</ErrorMessage>}>
+				{props.children}
+			</ErrorBoundary>
 		</div>
 	);
 }
