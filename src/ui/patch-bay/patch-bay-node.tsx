332a06d973e781945d289c2545501a24a729e2a4
Kav Singh <flakisms@gmail.com>
2023-08-01 23:47:30 +0200

    patchbay solid

--- src/ui/patch-bay/patch-bay-node.tsx
+++ src/ui/patch-bay/patch-bay-node.tsx
@@ -1,38 +1,39 @@
-import { memo } from "react";
+import { createMemo } from "solid-js";
 import { twMerge } from "tailwind-merge";
 
 import useConnection from "~/app-store/hooks/use-connection";
 
 import type { AudioNodeMeta } from "~/types";
 
-export default memo(function PatchBayNode({
-	source,
-	target,
-}: {
+export default function PatchBayNode(props: {
 	source: AudioNodeMeta;
 	target: AudioNodeMeta;
 }) {
 	const { connection, isBlocked, toggleConnection } = useConnection(
-		source,
-		target,
+		// eslint-disable-next-line solid/reactivity
+		props.source,
+		// eslint-disable-next-line solid/reactivity
+		props.target,
 	);
-	const color = connection?.color;
-	const title = isBlocked
-		? "This will cause a circular connection, big feedback, ear bleeding, much sadness"
-		: `From ${source.label} to ${target.label}`;
+	const color = createMemo(() => connection()?.color);
+	const title = createMemo(() => {
+		return isBlocked()
+			? "This will cause a circular connection, big feedback, ear bleeding, much sadness"
+			: `From ${props.source.label} to ${props.target.label}`;
+	});
 
 	return (
 		<button
-			title={title}
+			title={title()}
 			onClick={toggleConnection}
-			style={{ color: color ?? "currentcolor" }}
-			className={twMerge(
+			style={{ color: color() ?? "currentcolor" }}
+			class={twMerge(
 				"mx-auto mb-0 h-3 w-3 border p-0 transition-all",
-				!color && "border-text100",
-				!color && !isBlocked && "hover:border-text400",
-				isBlocked && "rotate-45 scale-50 cursor-default bg-text100",
-				!!color && !isBlocked && "border-current bg-current",
+				!color() && "border-text100",
+				!color() && !isBlocked() && "hover:border-text400",
+				isBlocked() && "rotate-45 scale-50 cursor-default bg-text100",
+				!!color() && !isBlocked() && "border-current bg-current",
 			)}
 		/>
 	);
-});
+}
