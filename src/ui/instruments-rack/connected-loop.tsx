9e157a34c6f15f77ebae186ba785d9c073a15118
Kav Singh <flakisms@gmail.com>
2023-08-01 23:34:05 +0200

    instruments rack solid

--- src/ui/instruments-rack/connected-loop.tsx
+++ src/ui/instruments-rack/connected-loop.tsx
@@ -1,5 +1,3 @@
-import { useCallback, memo } from "react";
-
 import {
 	receiveAudioFile as receiveAudioFileAction,
 	selectAudioFile as selectAudioFileAction,
@@ -14,7 +12,7 @@ import { nodeType, UI } from "~/nodes/instruments/loop";
 import type { NodeProps as Eq3Props } from "~/nodes/audio-effects/eq3";
 import type { NodeProps } from "~/nodes/instruments/loop";
 
-export default memo(function ConnectedLoop(props: { id: string }) {
+export default function ConnectedLoop(props: { id: string }) {
 	const {
 		label,
 		connections,
@@ -24,58 +22,51 @@ export default memo(function ConnectedLoop(props: { id: string }) {
 		updateLabel,
 		duplicate,
 		remove,
-	} = useAudioNode<NodeProps>(id, validateNodeType(nodeType));
+		// eslint-disable-next-line solid/reactivity
+	} = useAudioNode<NodeProps>(props.id, validateNodeType(nodeType));
 
 	const dispatch = useAppDispatch();
 
-	const receiveAudioFile = useCallback(
-		(file: File) => {
-			void dispatch(receiveAudioFileAction({ id, file }));
-		},
-		[id, dispatch],
-	);
+	function receiveAudioFile(file: File) {
+		void dispatch(receiveAudioFileAction({ id: props.id, file }));
+	}
 
-	const selectAudioFile = useCallback(() => {
-		void dispatch(selectAudioFileAction({ id }));
-	}, [id, dispatch]);
+	function selectAudioFile() {
+		void dispatch(selectAudioFileAction({ id: props.id }));
+	}
 
-	const handleGainChange = useCallback(
-		(gain: number) => updateAudioProps({ gain }),
-		[updateAudioProps],
-	);
+	function handleGainChange(gain: number) {
+		updateAudioProps({ gain });
+	}
 
-	const handlePlaybackRateChange = useCallback(
-		(playbackRate: number) => updateAudioProps({ playbackRate }),
-		[updateAudioProps],
-	);
+	function handlePlaybackRateChange(playbackRate: number) {
+		updateAudioProps({ playbackRate });
+	}
 
-	const handleEqChange = useCallback(
-		(eqProps: Eq3Props) => updateAudioProps(eqProps),
-		[updateAudioProps],
-	);
+	function handleEqChange(eqProps: Eq3Props) {
+		updateAudioProps(eqProps);
+	}
 
-	const handleLoopRegionChange = useCallback(
-		(loopStart: number, loopEnd: number) =>
-			updateAudioProps({ loopStart, loopEnd }),
-		[updateAudioProps],
-	);
+	function handleLoopRegionChange(loopStart: number, loopEnd: number) {
+		updateAudioProps({ loopStart, loopEnd });
+	}
 
 	return (
-		<NodeWrapper isActive={isActive}>
+		<NodeWrapper isActive={isActive()}>
 			<UI
-				nodeId={id}
-				loopStart={audioProps.loopStart}
-				loopEnd={audioProps.loopEnd}
-				label={label}
-				fileName={audioProps.fileName}
-				connections={connections}
-				isActive={isActive}
-				highGain={audioProps.highGain}
-				midGain={audioProps.midGain}
-				lowGain={audioProps.lowGain}
-				playbackRate={audioProps.playbackRate}
-				gain={audioProps.gain}
-				audioBuffer={audioProps.audioBuffer}
+				nodeId={props.id}
+				loopStart={audioProps().loopStart}
+				loopEnd={audioProps().loopEnd}
+				label={label()}
+				fileName={audioProps().fileName}
+				connections={connections()}
+				isActive={isActive()}
+				highGain={audioProps().highGain}
+				midGain={audioProps().midGain}
+				lowGain={audioProps().lowGain}
+				playbackRate={audioProps().playbackRate}
+				gain={audioProps().gain}
+				audioBuffer={audioProps().audioBuffer}
 				onGainChange={handleGainChange}
 				onPlaybackRateChange={handlePlaybackRateChange}
 				onEqChange={handleEqChange}
@@ -88,4 +79,4 @@ export default memo(function ConnectedLoop(props: { id: string }) {
 			/>
 		</NodeWrapper>
 	);
-});
+}
