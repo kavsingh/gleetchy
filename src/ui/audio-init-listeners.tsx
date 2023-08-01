11e96a366b617ffd6e9777ea7801e17a13ce0db6
Kav Singh <flakisms@gmail.com>
2023-08-01 23:16:41 +0200

    global listeners to solid

---
+++ src/ui/audio-init-listeners.tsx
@@ -0,0 +1,18 @@
+import { onCleanup } from "solid-js";
+
+import useAudioContext from "~/app-store/hooks/use-audio-context";
+
+export default function AudioInitListeners() {
+	const { initAudioContext } = useAudioContext();
+
+	globalThis.window.addEventListener("click", initAudioContext, {
+		passive: true,
+		once: true,
+	});
+
+	onCleanup(() => {
+		globalThis.window.removeEventListener("click", initAudioContext, true);
+	});
+
+	return null;
+}
