4dd4b46856cb0682110a0bb4e5169404c7a5ffb6
Kav Singh <flakisms@gmail.com>
2023-08-01 23:24:00 +0200

    masthead solid

--- src/ui/masthead/play-pause-button.tsx
+++ src/ui/masthead/play-pause-button.tsx
@@ -1,14 +1,12 @@
-import { memo } from "react";
-
 import useGlobalPlayback from "~/app-store/hooks/use-global-playback";
 import Button from "~/components/button";
 
-export default memo(function PlayPauseButton() {
+export default function PlayPauseButton() {
 	const { isPlaying, togglePlayback } = useGlobalPlayback();
 
 	return (
 		<Button tabIndex={0} onClick={togglePlayback} variant="text">
-			{isPlaying ? "Stop" : "Play"}
+			{isPlaying() ? "Stop" : "Play"}
 		</Button>
 	);
-});
+}
