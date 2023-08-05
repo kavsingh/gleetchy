12f5cd5d1d00e37753541f357289330ce2b92e58
Kav Singh <flakisms@gmail.com>
2023-08-05 20:18:23 +0200

    .git/eslint ignores

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,2 +1,2 @@
 /** @type {import('prettier').Config} */
-module.exports = require("./packages/codestyle-js/prettier.config.cjs")
+module.exports = require("./packages/codestyle-js/prettier.config.cjs");
