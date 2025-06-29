278952ab513e5409533f4f040d9d98155872b68d
Kav Singh <flakisms@gmail.com>
2025-06-29 20:50:59 +0200

    build(deps):  bump deps

--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,6 @@
 export default {
 	quoteProps: "consistent",
 	useTabs: true,
+	// https://github.com/prettier/prettier/issues/15956#issuecomment-3000347490
+	overrides: [{ files: ["**/*.jsonc"], options: { trailingComma: "none" } }],
 };
